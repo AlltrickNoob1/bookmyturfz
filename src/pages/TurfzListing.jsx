2b4ed015361da695d1ca3da5ac66cb7910038ae9
Ansari Abdullah <110453282+AlltrickNoob@users.noreply.github.com>
2026-07-08 16:35:49 +0530

    Refactor TurfzListing component structure

--- src/pages/TurfzListing.jsx
+++ src/pages/TurfzListing.jsx
@@ -4,19 +4,16 @@ import { TurfNav } from "../components/TurfNav";
 import { Turfdata } from "../components/Turfdata";
 import { Footer } from "../components/Footer";
 
-
-
 export const TurfzListing = () => {
+  const [turf, setTurf] = useState("All");
   const [search, setSearch] = useState("");
 
-<TurfNav
-  setTurf={setTurf}
-  onSearchChange={setSearch}
-  search={search}
-/>
-
-<Turfdata
-  turf={turf}
-  search={search}
-/>
+  return (
+    <div id="mainContainer">
+      <TurfNav setTurf={setTurf} onSearchChange={setSearch} search={search} />
+      {/* <MapContainer/> */}
+      <Turfdata turf={turf} search={search} />
+      <Footer />
+    </div>
+  );
 };
